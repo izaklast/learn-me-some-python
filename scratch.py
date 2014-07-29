ad2cf9765e018902ef69b832a0bb495954b54124
izaklast <izaklast@gmail.com>
2014-07-29 09:51:31 -0700

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,11 +1,11 @@
 timbitsLeft = int(input()) # step 1: get the input
 totalCost = 0              # step 2: initialize the total cost
 
-if timbitsLeft >= 40:
+
 # step 3: buy as many large boxes as you can
-    bigBoxes = int(timbitsLeft / 40)
-    totalCost = totalCost + (bigBoxes * 6.19)    # update the total price
-    timbitsLeft = timbitsLeft - (40 * bigBoxes)  # calculate timbits still needed
+bigBoxes = int(timbitsLeft / 40)
+totalCost = totalCost + (bigBoxes * 6.19)    # update the total price
+timbitsLeft = timbitsLeft - (40 * bigBoxes)  # calculate timbits still needed
 
 if timbitsLeft >= 20:                # step 4, can we buy a medium box?
     totalCost = totalCost + 3.39
