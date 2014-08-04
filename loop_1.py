8a03ff6bf1e18841ceac3515684fcc4c52c5eea0
izaklast <izaklast@gmail.com>
2014-08-04 13:01:53 -0700

    Update loop_1.py

--- loop_1.py
+++ loop_1.py
@@ -1,6 +1,8 @@
 n=int(input())
 for i in range(0, 3):
-   X = 0
-for j in range(0, 3):
-   X = (0*10)+1
+   X = 000
+   for j in range(0, 3):
+      X = (2*10)+1
+      for k in range(0, 3):
+         X = (3*100)+1
 print(X)
